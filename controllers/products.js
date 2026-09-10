d20e9b55e4a389b3a9f44b8d1ef92bb35d21336c
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-09-10 23:05:07 +0545

    4.1

--- controllers/products.js
+++ controllers/products.js
@@ -3,5 +3,5 @@ const getAllProducts = async (req, res) => {
 };
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({msg:"I am getAllProducts"});
+    res.status(200).json({msg:"I am getAllProductsTesting"});
 };
